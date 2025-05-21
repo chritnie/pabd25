982c904986c15deeeb45df86f4442dfe904b7789
Кирилл Журавлев <k.zhuravlev@atomgroup.io>
2025-05-21 21:42:06 +0300

    add dvc, separate lifecycle.py to 4 files for each method

--- src/parse_cian.py
+++ src/parse_cian.py
@@ -1,36 +1,32 @@
-"""  Parse data from cian.ru
-https://github.com/lenarsaitov/cianparser
-"""
 import datetime
-
-import cianparser
+import os
 import pandas as pd
+import cianparser
+import argparse
 
-moscow_parser = cianparser.CianParser(location="Москва")
 
-
-def main():
-    """
-    Function docstring
-    """
+def parse_cian(n_rooms: int):
+    os.makedirs("src/data/raw", exist_ok=True)
+    parser = cianparser.CianParser(location="Москва")
     t = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-    n_rooms = 1
-    csv_path = f'data/raw/{n_rooms}_{t}.csv'
-    data = moscow_parser.get_flats(
+    csv_path = f"src/data/raw/{n_rooms}_{t}.csv"
+
+    data = parser.get_flats(
         deal_type="sale",
-        rooms=(1, 2, 3),
+        rooms=(n_rooms,),
         with_saving_csv=False,
         additional_settings={
             "start_page": 1,
-            "end_page": 10,
-            "object_type": "secondary"
-        })
+            "end_page": 2,
+            "object_type": "secondary",
+        },
+    )
     df = pd.DataFrame(data)
-
-    df.to_csv(csv_path,
-              encoding='utf-8',
-              index=False)
+    df.to_csv(csv_path, encoding="utf-8", index=False)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_rooms", type=int, default=1)
+    args = parser.parse_args()
+    parse_cian(args.n_rooms)
